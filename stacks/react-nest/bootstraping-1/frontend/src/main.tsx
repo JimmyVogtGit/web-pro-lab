3d2628ed3f7d089e7a8cfa9cfc94aedd22b2a25b
JimmyVogtGit <vogt.jimmy18@gmail.com>
2025-10-31 20:24:13 +0100

    feat: add tanstack query and devtools

--- stacks/react-nest/bootstraping-1/frontend/src/main.tsx
+++ stacks/react-nest/bootstraping-1/frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools />
+    <StrictMode>
+      //StrictMode for dev mode
+      <App />
+    </StrictMode>
+  </QueryClientProvider>
+);
