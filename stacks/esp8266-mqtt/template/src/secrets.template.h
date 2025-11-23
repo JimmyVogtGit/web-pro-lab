743134236ff1a6b44511066cd4087f5c2bcb23a6
JimmyVogtGit <vogt.jimmy18@gmail.com>
2025-11-23 23:00:25 +0100

    feat: add secrets template

---
+++ stacks/esp8266-mqtt/template/src/secrets.template.h
@@ -0,0 +1,6 @@
+#define SECRET_SSID "my_ssid"
+#define SECRET_PASS "my_wifi_password"
+#define SECRET_MQTT_SERVER "mqtt_server"
+#define SECRET_MQTT_USER "mqtt_user"
+#define SECRET_MQTT_PASS "mqtt_password"
+
